7c27e801987fb1a7d5785f10f81f37bb8ce0038f
Abhishek Agrawal <158209043+9575509537@users.noreply.github.com>
2024-04-26 13:31:23 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -2,6 +2,22 @@ function longestSubstring(s) {
 
 
     // Implementation of longestSubstring function
+    function smallestMissingPositiveInteger(nums) {
+
+
+        // Implement the function smallest_missing_positive_integer
+        nums.sort();
+        let number = 1;
+        let i = 0;
+        while(i<nums.length && nums[i]<=0) i++;
+        for(i; i<nums.length; i++) {
+          if(nums[i] != number) return number;
+          number++;
+        }
+        return number;
+      }
+      
+      module.exports = smallestMissingPositiveInteger;
 }
 
 module.exports = { longestSubstring };
